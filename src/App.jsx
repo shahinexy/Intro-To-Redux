8c2d4816ca9fd89c4cff18b6a01dbf25c919a133
shahinexy <shahinexy@gmail.com>
2025-01-30 13:22:30 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h2>Counter With Redux</h2>
+      <button>Increment</button>
+      <div>0</div>
+      <button>Decrement</button>
+    </>
+  );
+}
+
+export default App;
